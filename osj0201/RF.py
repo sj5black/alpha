d6db7a270370608c30207a5d3e0d7fc4d8861f77
osj0201 <osj020201@gmail.com>
2024-10-24 20:07:31 +0900

    osj0201

--- osj0201/RF.py
+++ osj0201/RF.py
@@ -1,3 +1,14 @@
 import seaborn as sns
 
 titanic = sns.load_dataset('titanic')
+
+print(titanic.head(5))
+# 생존(Survived): 0 = 죽은사람, 1 = 살아남은사람
+# 객실등급(pclass): 1등급 = 1, 2등급 = 2, 3등급 = 3
+# 성별(sex): male = 남자, female = 여자
+# 나이(age)
+
+
+
+
+print(titanic.describe())
