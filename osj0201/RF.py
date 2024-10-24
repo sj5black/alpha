f77efacebb4a8f7de5ff320b6f8c494b0b3bd9e5
osj0201 <osj020201@gmail.com>
2024-10-24 20:38:34 +0900

    osj0201

--- osj0201/RF.py
+++ osj0201/RF.py
@@ -6,7 +6,9 @@ print(titanic.head(5))
 # 생존(Survived): 0 = 죽은사람, 1 = 살아남은사람
 # 객실등급(pclass): 1등급 = 1, 2등급 = 2, 3등급 = 3
 # 성별(sex): male = 남자, female = 여자
-# 나이(age)
+# 나이(age) 
+# 함께 탑승한 형제 또는 배우자 수(sibsp)
+# 함께 탑승한 부모 또는 자녀 수(parch)
 
 
 
